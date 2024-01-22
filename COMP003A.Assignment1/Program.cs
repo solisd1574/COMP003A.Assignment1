dd4e90a9914f04df81898138e86613c270462269
david <david@DESKTOP-BQHLQ1S>
2024-01-21 23:42:21 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,17 @@
+﻿// this is a single-line comment or
+/* this is a multi-line comment
+*
+*   Author:    Jonathan Cruz
+*   Course:    COMP-003A-L01
+*   Purpose:   Code for Assignment 1
+*/
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+        }
+    }
+}
