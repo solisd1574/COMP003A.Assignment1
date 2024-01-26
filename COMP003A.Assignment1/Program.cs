845231123a50ce5956cf8f6aeb4424d08518c2a0
David Solis <davidsolis1574@my.whccd.edu>
2024-01-25 17:38:28 -0800

    initial commmit

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -1,9 +1,9 @@
-﻿// this is a single-line comment or
+﻿// this is a single-line comment
 /* this is a multi-line comment
 *
-*   Author:    Jonathan Cruz
-*   Course:    COMP-003A-L01
-*   Purpose:   Code for Assignment 1
+*Author:    Jonathan Cruz
+*Course:    COMP-003A-L01
+*Purpose:   Code for Assignment 1
 */
 namespace COMP003A.Assignment1
 {
@@ -12,6 +12,106 @@ namespace COMP003A.Assignment1
         static void Main(string[] args)
         {
             Console.WriteLine("100 Things About Me");
+            Console.WriteLine("1. Shy");
+            Console.WriteLine("2. Bored");
+            Console.WriteLine("3. Curious");
+            Console.WriteLine("4. Organized");
+            Console.WriteLine("5. Dependable");
+            Console.WriteLine("6. Bright");
+            Console.WriteLine("7. Tall");
+            Console.WriteLine("8. Outgoing");
+            Console.WriteLine("9. Grateful");
+            Console.WriteLine("10. Hiker");
+            Console.WriteLine("11. Traveler");
+            Console.WriteLine("12. Artist");
+            Console.WriteLine("13. Creative");
+            Console.WriteLine("14. Prepared");
+            Console.WriteLine("15. Introvert");
+            Console.WriteLine("16. Timid");
+            Console.WriteLine("17. Progressive");
+            Console.WriteLine("18. Understanding");
+            Console.WriteLine("19. Thoughtful");
+            Console.WriteLine("20. Trustworthy");
+            Console.WriteLine("21. Efficient");
+            Console.WriteLine("22. Focused");
+            Console.WriteLine("23. Daring");
+            Console.WriteLine("24. Equipped");
+            Console.WriteLine("25. Compeling");
+            Console.WriteLine("26. Qualified");
+            Console.WriteLine("27. Skilled");
+            Console.WriteLine("28. Presistent");
+            Console.WriteLine("29. Clever");
+            Console.WriteLine("30. Considerate");
+            Console.WriteLine("31. Sincere");
+            Console.WriteLine("32. Chill");
+            Console.WriteLine("33. Joyful");
+            Console.WriteLine("34. Energetic");
+            Console.WriteLine("35. Friendly");
+            Console.WriteLine("36. Open-minded");
+            Console.WriteLine("37. Intelligent");
+            Console.WriteLine("38. Modest");
+            Console.WriteLine("39. Kind");
+            Console.WriteLine("40. Adaptable");
+            Console.WriteLine("41. Funny");
+            Console.WriteLine("42. Resourceful");
+            Console.WriteLine("43. Adventurous");
+            Console.WriteLine("44. Loving");
+            Console.WriteLine("45. Flexible");
+            Console.WriteLine("46. Charming");
+            Console.WriteLine("47. Exclusive");
+            Console.WriteLine("48. Intellectual");
+            Console.WriteLine("49. Kind");
+            Console.WriteLine("50. Neat");
+            Console.WriteLine("51. Patient");
+            Console.WriteLine("52. Positive");
+            Console.WriteLine("53. Polite");
+            Console.WriteLine("54. Quick");
+            Console.WriteLine("55. Responsive");
+            Console.WriteLine("56. Fat");
+            Console.WriteLine("57. Strong");
+            Console.WriteLine("58. Logical");
+            Console.WriteLine("59. Fidgety");
+            Console.WriteLine("60. Lucky");
+            Console.WriteLine("61. Wakeful");
+            Console.WriteLine("62. Concern");
+            Console.WriteLine("63. Passive");
+            Console.WriteLine("64. Brave");
+            Console.WriteLine("65. Depressed");
+            Console.WriteLine("66. Happy");
+            Console.WriteLine("67. Clean");
+            Console.WriteLine("68. Reader");
+            Console.WriteLine("69. Gamer");
+            Console.WriteLine("70. Collecter");
+            Console.WriteLine("71. Spender");
+            Console.WriteLine("72. Joker");
+            Console.WriteLine("73. Guitarist");
+            Console.WriteLine("74. Passive");
+            Console.WriteLine("75. Straight-Forward");
+            Console.WriteLine("76. Different");
+            Console.WriteLine("77. Caustious");
+            Console.WriteLine("78. Caring");
+            Console.WriteLine("79. Direct");
+            Console.WriteLine("80. Determined");
+            Console.WriteLine("81. Coordinated");
+            Console.WriteLine("82. Planner");
+            Console.WriteLine("83. Analytical");
+            Console.WriteLine("84. Driven");
+            Console.WriteLine("85. Bold");
+            Console.WriteLine("86. Realist");
+            Console.WriteLine("87. Responsible");
+            Console.WriteLine("88. Humorous");
+            Console.WriteLine("89. Imaginative");
+            Console.WriteLine("90. Knowledgabe");
+            Console.WriteLine("91. Polite");
+            Console.WriteLine("92. Discreet");
+            Console.WriteLine("93. Detailed");
+            Console.WriteLine("94. Agreeable");
+            Console.WriteLine("95. Amusing");
+            Console.WriteLine("96. Powerful");
+            Console.WriteLine("97. Formal");
+            Console.WriteLine("98. Genuine");
+            Console.WriteLine("99. Good");
+            Console.WriteLine("100. Expert");
         }
     }
 }
